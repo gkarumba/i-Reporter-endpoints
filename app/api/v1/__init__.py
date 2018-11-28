c65f1d5305e6a8ab0d70d3162a7a144c2c9b00fe
iamrwaga <iamrwaga@gmail.com>
2018-11-28 09:26:49 +0300

    [finishes #162261259] Modify the init file

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,4 +1,4 @@
-from views import ReportList
+from views import ReportLists
 
 from flask import Blueprint
 from flask_restful import Api, Resource
@@ -7,4 +7,4 @@ version_one = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
 api = Api(version_one)
 
-api.add_resource(ReportList, '/reports')
+api.add_resource(ReportLists, '/reports')
