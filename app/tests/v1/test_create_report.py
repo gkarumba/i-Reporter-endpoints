0798ba79220b9580effcf9fd46ff64095c090435
iamrwaga <iamrwaga@gmail.com>
2018-12-01 09:28:03 +0300

    [#162298730] Add the delete test

--- app/tests/v1/test_create_report.py
+++ app/tests/v1/test_create_report.py
@@ -58,7 +58,7 @@ class TestReportCreation(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIn('Report editted successfully', str(result))
 
-    def test_DELETE_remove_report(self):
+    def test_DELETE_report(self):
         self.app.post('/api/v1/reports', data=json.dumps(self.data), content_type='application/json')
         response = self.app.delete('/api/v1/reports/1')
         result = json.loads(response.data)
