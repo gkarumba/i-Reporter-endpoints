cc80f4cf7a78e44ef04a0e113f818c4fd1dd8d53
gkarumba <gachegua@gmail.com>
2018-12-13 12:59:51 +0300

    [#162632198] Adds changes to the folder structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint
 #local imports
 from instance import config
-# from app.api.incidents.v1 import version_one as v1
+from app.api.incidents.v1 import version_one as v1
 from app.api.users.v1 import user
 from app.api.users.v2 import users2
 from app.api.incidents.v2 import version_two as v2
--- app/api/incidents/v2/__init__.py
+++ app/api/incidents/v2/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from app.api.v2.views import ReportList,EditReport,DeleteReport,GetSingleReport
+from app.api.incidents.v2.views import ReportList,EditReport,DeleteReport,GetSingleReport
 
 version_two = Blueprint('api_v2',__name__, url_prefix='/api/v2')
 
